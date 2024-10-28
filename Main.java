4f7cca010f822b3197b81d9261ff776b69bcd113
m020202 <m020202@naver.com>
2024-10-28 14:46:07 +0900

    feat: Function<T,R> EG

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        Shape shape = new Rectangle();
-        shape.draw();
-        System.out.println("hello world");
+        Function<String, Integer> toInt = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) {
+                return Integer.parseInt(s);
+            }
+        };
+
+        Integer number = toInt.apply("100");
+        System.out.println("number: " + number);
+
     }
 }
