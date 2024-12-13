0599229d1e3799028e670c2659613ddb770ff8d1
m020202 <m020202@naver.com>
2024-12-13 16:39:49 +0900

    Feat: Decorator Pattern EG using Toast & ToppingDecorator

--- decorator_pattern/App.java
+++ decorator_pattern/App.java
@@ -2,9 +2,10 @@ package decorator_pattern;
 
 public class App {
     public static void main(String[] args) {
-        Toast toast1 = new NormalBread();
+        Toast toast1 = new MilkBread();
         toast1 = new Ham(toast1);
         toast1 = new Cheeze(toast1);
+        toast1 = new Vegetable(toast1);
 
         System.out.println("주문한 토스트: " + toast1.getName());
         System.out.println("칼로리: " + toast1.getKcal());
