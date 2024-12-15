74a4ddc3f8b871f8f2daaccee67ae86d3321293b
m020202 <m020202@naver.com>
2024-12-15 18:20:41 +0900

    Feat: java_lotto_6 초기 세팅

---
+++ java_lotto_6/Application.java
@@ -0,0 +1,9 @@
+package java_lotto_6;
+
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java_lotto_6/Lotto.java
@@ -0,0 +1,18 @@
+package java_lotto_6;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
